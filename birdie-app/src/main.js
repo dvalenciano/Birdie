1d4b048b5c7940f257fa28b14077004d106f218f
Derek <dvalenciano1986@gmail.com>
2021-10-14 14:07:05 -0500

    added bootstrap plugin in main

--- birdie-app/src/main.js
+++ birdie-app/src/main.js
@@ -4,6 +4,9 @@ import { BootstrapVue, LayoutPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+Vue.use(LayoutPlugin)
+
 new Vue({
   render: h => h(App)
 }).$mount('#app')
