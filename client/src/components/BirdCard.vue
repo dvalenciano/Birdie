61a5a149b3587d21ac8b3ad5813e7f0d6d83feb2
Derek <dvalenciano1986@gmail.com>
2021-10-14 14:40:47 -0500

    created bird card

---
+++ client/src/components/BirdCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-card bg-variant="light" :header="post.username" class="text-center">
+        <b-card-text>{{post.post}}</b-card-text>
+        <DeletePop :postId="post.id" :key="post.id"/>
+    </b-card>
+  </div>
+</template>
+
+<script>
+import DeletePop from './DeletePop.vue'
+export default {
+  components: {DeletePop},
+  name: 'PostCard',
+  props: ['post'],
+  methods: {
+  }
+}
+</script>
