bc87413eadbaf9e5afca4b5b512bb5144b6121cb
Derek <dvalenciano1986@gmail.com>
2021-10-16 16:06:21 -0500

    finished updatecomment and created comment delete

---
+++ client/src/components/CommentCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-card bg-variant="light" :header="comment.comment" class="text-center">
+        <b-card-text>{{comment.comment}}</b-card-text>
+        <div>
+          <!-- <DeletePop :commentId="comment.id" /> -->
+        </div>
+        <div>
+          <CommentModal :commentId="comment.id" />
+        </div>
+    </b-card>
+  </div>
+</template>
+
+<script>
+// import DeletePop from './DeletePop.vue'
+import CommentModal from './CommentModal.vue'
+export default {
+  components: { CommentModal},
+  name: 'CommentCard',
+  props: ['comment'],
+  methods: {
+  }
+}
+</script>
