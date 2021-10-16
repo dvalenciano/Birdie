c1195035f5697319e120e34cf10646ac7e818a76
Derek <dvalenciano1986@gmail.com>
2021-10-16 14:56:31 -0500

    updated router js to accomadate home and about page

--- client/src/router.js
+++ client/src/router.js
@@ -1,12 +1,9 @@
 import VueRouter from 'vue-router'
-import HomePage from 
+import HomePage from './pages/HomePage'
 import About from './pages/About'
 
 const routes = [
-  { path: '/', component: Home, name: 'Home' },
-  { path: '/details/:game_id', component: GameDetails, name: 'GameDetails' },
-  { path: '/genre/:genre_id', component: GameGenre, name: 'GameGenre' },
-  { path: '/games', component: ViewGames, name: 'ViewGames' },
+  { path: '/', component: HomePage, name: 'HomePage' },
   { path: '/about', component: About, name: 'About' }
 ]
 
