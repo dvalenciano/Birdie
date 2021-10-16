192261310d9801bd81cc157504a9c2021e32b7b8
Derek <dvalenciano1986@gmail.com>
2021-10-16 15:37:49 -0500

    created Comment page in vue

---
+++ client/src/pages/CommentPage.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div>
+      <div class="nav-bar">
+        <!-- <b-nav class="logo-text">
+          <div class="grid-container">
+            <h3 class="nav-h2">Birdie</h3>
+        
+          </div>
+        
+        </b-nav> -->
+        <form v-on:submit="getSearchResults" @submit.prevent>
+          <b-input-group class="mt-1">
+            <template #append >
+              <b-button type="submit">Search</b-button>
+            </template>
+            <b-form-input type="search" sm="3" v-model="searchQuery" :value='searchQuery'></b-form-input>
+          </b-input-group>
+        </form>
+      </div>
+      
+    </div>
+    <h2>Birds</h2>
+    <Modal />
+    <section v-if="searched === false">
+      <BirdCard v-for="bird in birds" :bird="bird" :key="bird.id" />
+    </section>
+    <section v-else>
+      <BirdCard v-for="bird in searchResults" :color="color" :bird="bird" :key="bird.id" />
+    </section>
+  </div>
+</template>
+
+
+<script>
+import axios from 'axios'
+import BirdCard from '../components/BirdCard.vue'
+import { BASE_URL } from '../globals'
+import Modal from '../components/Modal.vue'
+// import { b-nav } from 'bootstrap-vue'
+export default {
+  name: 'Home',
+  components: {
+    BirdCard,
+    Modal
+  },
+  data: () => ({
+    birds: [],
+    searchQuery: '',
+    searchResults: [],
+    searched: false
+  }),
+  mounted: function() {
+    this.getBirds()
+  },
+  methods: {
+    async getBirds() {
+      const res = await axios.get(`${BASE_URL}/birds`)
+      this.birds = res.data
+    },
+   
+    getSearchResults(){
+      let birds = this.birds
+      let result = birds.filter(bird => bird.bird.includes(this.searchQuery))
+      console.log(result)
+      this.searchResults = result
+      this.searched = true
+    }
+  }
+}
+</script>
