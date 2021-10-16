bd15c50fde66d7bce28b717e1b46cef82e283a53
Derek <dvalenciano1986@gmail.com>
2021-10-16 15:24:14 -0500

    created location model

---
+++ models/location.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from models.db import db
+
+
+class Location(db.Model):
+    __tablename__ = 'locations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(255), nullable=False, unique=False)
+    note = db.Column(db.String(255), nullable=False, unique=False)
+    created_at = db.Column(db.DateTime, default=str(
+        datetime.utcnow()), nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow(
+    ), nullable=False, onupdate=datetime.now())
+
+    def __init__(self, location, note):
+        self.location = location
+        self.note = note
+
+    def json(self):
+        return {"id": self.id, "comment": self.comment, "bird_id": self.bird_id, "created_at": str(self.created_at), "updated_at": str(self.updated_at)}
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    @classmethod
+    def find_all(cls):
+        comments = Comment.query.all()
+        return [c.json() for c in comments]
+
+    @classmethod
+    def find_by_id(cls, comment_id):
+        comment = Comment.query.filter_by(id=comment_id).first()
+        return comment
