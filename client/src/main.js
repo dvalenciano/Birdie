57ac96ce72cce0ac842a6c0849cca8e3097e32fc
Derek <dvalenciano1986@gmail.com>
2021-10-16 14:58:34 -0500

    update to main.js to activate router

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import router from './router'
 import { BootstrapVue, LayoutPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -9,7 +11,9 @@ Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(LayoutPlugin)
+Vue.use(VueRouter)
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
