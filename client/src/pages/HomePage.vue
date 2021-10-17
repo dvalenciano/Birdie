06566d71c2bafb7b0c397c4540d77a151b2fc649
Derek <dvalenciano1986@gmail.com>
2021-10-17 15:47:50 -0500

    updated home page

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,32 +1,9 @@
 <template>
   <div>
-    <div>
-      <div class="nav-bar">
-        <!-- <b-nav class="logo-text">
-          <div class="grid-container">
-            <h3 class="nav-h2">Birdie</h3>
-        
-          </div>
-        
-        </b-nav> -->
-        <form v-on:submit="getSearchResults" @submit.prevent>
-          <b-input-group class="mt-1">
-            <template #append >
-              <b-button type="submit">Search</b-button>
-            </template>
-            <b-form-input type="search" sm="3" v-model="searchQuery" :value='searchQuery'></b-form-input>
-          </b-input-group>
-        </form>
-      </div>
-      
-    </div>
     <h2>Birds</h2>
     <Modal />
-    <section v-if="searched === false">
+    <section>
       <BirdCard v-for="bird in birds" :bird="bird" :key="bird.id" />
-    </section>
-    <section v-else>
-      <BirdCard v-for="bird in searchResults" :color="color" :bird="bird" :key="bird.id" />
     </section>
   </div>
 </template>
@@ -57,14 +34,6 @@ export default {
     async getBirds() {
       const res = await axios.get(`${BASE_URL}/birds`)
       this.birds = res.data
-    },
-   
-    getSearchResults(){
-      let birds = this.birds
-      let result = birds.filter(bird => bird.bird.includes(this.searchQuery))
-      console.log(result)
-      this.searchResults = result
-      this.searched = true
     }
   }
 }
