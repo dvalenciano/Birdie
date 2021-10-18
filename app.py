acf70b22d6705de5f76aa24c6e1689ba9289fe31
Derek <dvalenciano1986@gmail.com>
2021-10-18 10:06:31 -0500

    uploaded heroku and changed api configs for deployment

--- app.py
+++ app.py
@@ -5,15 +5,26 @@ from flask_migrate import Migrate
 from models.db import db
 from models import user, bird, comment, location
 from resources import user, bird, comment, location
+import os
 
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/birdie_db"
-app.config['SQLALCHEMY_ECHO'] = True
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL.replace(
+        "://", "ql://", 1)
+    app.config['SQLALCHEMY_ECHO'] = False
+    app.env = 'production'
+else:
+    app.debug = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432/birdie_db'
+    app.config['SQLALCHEMY_ECHO'] = True
+
 
 db.init_app(app)
 migrate = Migrate(app, db)
@@ -28,4 +39,4 @@ api.add_resource(location.Locations, '/locations')
 api.add_resource(location.LocationDetail, '/locations/<int:location_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
